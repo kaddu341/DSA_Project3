e5527ec20116cf905c57c62e85806241df60fa52
kaddu341 <awwabaliazam@gmail.com>
2024-11-27 11:06:03 -0500

    Streamlining parsing functionality.

--- main.cpp
+++ main.cpp
@@ -12,14 +12,38 @@
 
 using namespace std;
 
-// declare helper function to process data
+// declare helper functions to process data
 vector<tuple<string, string, float>> parseDataset(const string& wordsFile, const string& bigramsFile);
 
+// main function
+int main() {
+    // "../" for parent directory in relative path
+    const vector<tuple<string, string, float>> edges = parseDataset("../data_processing/eng-simple_wikipedia_2021_300K/eng-simple_wikipedia_2021_300K-words.txt", "../data_processing/eng-simple_wikipedia_2021_300K/eng-simple_wikipedia_2021_300K-co_n.txt");
 
+    auto Alist = AdjacencyList(edges);
 
+    // testing
+    auto indegreeList = Alist.indegrees("the");
+    auto outdegreeList = Alist.outdegrees("the");
+    for (const auto& [key, value] : indegreeList) {
+        cout << "InDegree Key: " << key << ", Value: " << value << endl;
+    }
+    for (const auto& [key, value] : outdegreeList) {
+        cout << "OutDegree Key: " << key << ", Value: " << value << endl;
+    }
 
-// define the helper function
-vector<tuple<string, string, float>> parseDatasetWindows(const string& wordsFile, const string& bigramsFile) {
+  /*
+    const tuple<string, string, float>& test = edges.at(79);
+    cout << get<0>(test) << endl;
+    cout << get<1>(test) << endl;
+    cout << get<2>(test) << endl;
+  */
+
+    return 0;
+}
+
+// define the helper functions
+vector<tuple<string, string, float>> parseDataset(const string& wordsFile, const string& bigramsFile) {
 
   // get all the words and their indices and store them in an unordered map
   unordered_map<int, string> wordsMap;
@@ -70,89 +94,4 @@ vector<tuple<string, string, float>> parseDatasetWindows(const string& wordsFile
 
   // return the result
   return edges;
-}
-
-// define the helper function
-vector<tuple<string, string, float>> parseDataset(const string& wordsFile, const string& bigramsFile) {
-  string cwd = filesystem::current_path().string();
-  cwd = cwd.substr(0, cwd.find_last_of('/'));
-
-  // get all the words and their indices and store them in an unordered map
-  unordered_map<int, string> wordsMap;
-  ifstream wordStream(cwd + "/" + wordsFile);
-  string line;
-
-  while (getline(wordStream, line)) {
-
-    // break up each line into words
-    istringstream iss(line);
-    vector<string> entries;
-    string entry;
-    while (iss >> entry) {
-      entries.push_back(entry);
-    }
-
-    // exception handling to avoid mis-processing data
-    if (entries.size() == 3) {
-      wordsMap[stoi(entries[0])] = entries[1];
-    }
-  }
-  wordStream.close();
-
-  // now process all the bigrams and store them in the return vector
-  vector<tuple<string, string, float>> edges;
-  ifstream bigramStream(cwd + "/" + bigramsFile);
-  while (getline(bigramStream, line)) {
-
-    // break up each line into words
-    istringstream iss(line);
-    vector<string> entries;
-    string entry;
-    while (iss >> entry) {
-      entries.push_back(entry);
-    }
-
-    // exception handling to avoid mis-processing data
-    if (entries.size() == 4) {
-      int index1 = stoi(entries[0]);
-      int index2 = stoi(entries[1]);
-
-      if ((wordsMap.find(index1) != wordsMap.end()) && (wordsMap.find(index2) != wordsMap.end())) {
-        edges.emplace_back(wordsMap[stoi(entries[0])], wordsMap[stoi(entries[1])], stof(entries[3]));
-      }
-    }
-  }
-  bigramStream.close();
-
-  // return the result
-  return edges;
-}
-
-
-
-// main function
-int main() {
-    // "../" for windows relative path
-    const vector<tuple<string, string, float>> edges = parseDatasetWindows("data_processing/eng-simple_wikipedia_2021_300K/eng-simple_wikipedia_2021_300K-words.txt", "data_processing/eng-simple_wikipedia_2021_300K/eng-simple_wikipedia_2021_300K-co_n.txt");
-
-    auto Alist = AdjacencyList(edges);
-
-
-
-    // testing
-    auto indegreeList = Alist.indegrees("the");
-    auto outdegreeList = Alist.outdegrees("the");
-    for (const auto& [key, value] : indegreeList) {
-        cout << "InDegree Key: " << key << ", Value: " << value << endl;
-    }
-    for (const auto& [key, value] : outdegreeList) {
-        cout << "OutDegree Key: " << key << ", Value: " << value << endl;
-    }
-
-//    const tuple<string, string, float>& test = edges.at(79);
-//    cout << get<0>(test) << endl;
-//    cout << get<1>(test) << endl;
-//    cout << get<2>(test) << endl;
-
-    return 0;
 }
